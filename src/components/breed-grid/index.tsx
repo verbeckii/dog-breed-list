4aecd18886b6b5145e7e0a15f8bf9157bdcb9b33
Dmitrii Verbeckii <verbeckii@gmail.com>
2023-11-04 23:37:38 +0200

    implemented infiniti load

--- src/components/breed-grid/index.tsx
+++ src/components/breed-grid/index.tsx
@@ -1,12 +1,16 @@
+"use client"
 import React from "react"
 import DogImage from "@/components/dog-image"
 import { useSearchParams } from "next/navigation"
+import { useState, useRef, useEffect } from "react"
 
 interface BreedGridProps {
 	breeds: { [key: string]: string[] }
 }
 
 const BreedGrid = ({ breeds }: BreedGridProps) => {
+	const [visibleCards, setVisibleCards] = useState(20)
+	const lastVisibleCardRef = useRef(null)
 	const searchParams = useSearchParams()
 	const selectedBreed = searchParams.get("breed")
 	const selectedSubBreed = searchParams.get("subBreed")
@@ -29,17 +33,61 @@ const BreedGrid = ({ breeds }: BreedGridProps) => {
 		}
 	}
 
+	useEffect(() => {
+		const observer = new IntersectionObserver(
+			(entries) => {
+				if (entries[0].isIntersecting) {
+					// Load more cards when the last card is in the viewport
+					setVisibleCards((prevVisibleCards) => prevVisibleCards + 20)
+				}
+			},
+			{
+				root: null,
+				rootMargin: "0px",
+				threshold: 0.1,
+			},
+		)
+		const lastCardRef = lastVisibleCardRef.current
+
+		if (lastCardRef) {
+			observer.observe(lastCardRef)
+		}
+
+		return () => {
+			if (lastCardRef) {
+				observer.unobserve(lastCardRef)
+			}
+		}
+	}, [lastVisibleCardRef])
+
 	return (
 		<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-			{Object.keys(allBreeds).map((breed) => (
-				<div
-					key={breed}
-					className="bg-white rounded-lg shadow-md p-4 flex flex-col items-center justify-evenly"
-				>
-					<DogImage breed={breed} />
-					<p className="mt-2 text-center">{breed}</p>
-				</div>
-			))}
+			{Object.keys(allBreeds)
+				.slice(0, visibleCards)
+				.map((breed, index) => {
+					// Attach a ref to the last visible card
+					if (index === visibleCards - 1) {
+						return (
+							<div
+								key={breed}
+								ref={lastVisibleCardRef}
+								className="bg-white rounded-lg shadow-md p-4 flex flex-col items-center justify-evenly"
+							>
+								<DogImage breed={breed} />
+								<p className="mt-2 text-center">{breed}</p>
+							</div>
+						)
+					}
+					return (
+						<div
+							key={breed}
+							className="bg-white rounded-lg shadow-md p-4 flex flex-col items-center justify-evenly"
+						>
+							<DogImage breed={breed} />
+							<p className="mt-2 text-center">{breed}</p>
+						</div>
+					)
+				})}
 		</div>
 	)
 }
