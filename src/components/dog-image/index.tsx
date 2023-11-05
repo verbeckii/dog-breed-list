b8b7117fb06743cf09fa1c0796c2e0e2a39857a2
Dmitrii Verbeckii <verbeckii@gmail.com>
2023-11-05 23:05:59 +0200

    fix: add 'not_found' placeholder for the image component

--- src/components/dog-image/index.tsx
+++ src/components/dog-image/index.tsx
@@ -1,31 +1,34 @@
 "use client"
 import Image from "next/image"
-import { useEffect, useState } from "react"
+import { useCallback, useEffect, useState } from "react"
 
 interface DogImageProps {
 	breed: string
 }
+const NOT_FOUND = "/static/images/not_found.png"
 
 export const DogImage = ({ breed }: DogImageProps) => {
-	const [src, setSrc] = useState(null)
+	const [src, setSrc] = useState<string | null>(null)
 	const [loading, setLoading] = useState(true)
-	const [error, setError] = useState<Error | null>(null)
+	const onError = useCallback(() => {
+		setSrc(NOT_FOUND)
+	}, [])
 
 	useEffect(() => {
 		const fetchDogImage = async () => {
 			setLoading(true)
-			setError(null)
 			try {
 				const response = await fetch(`https://dog.ceo/api/breed/${breed}/images/random`)
 				if (!response.ok) {
+					setSrc(NOT_FOUND)
 					throw new Error("Network response was not ok")
 				}
 				const data = await response.json()
 				setSrc(data.message)
 				setLoading(false)
 			} catch (error) {
-				const typedError = error as Error
-				setError(typedError)
+				console.log("error: ", error)
+				setSrc(NOT_FOUND)
 				setLoading(false)
 			}
 		}
@@ -37,14 +40,21 @@ export const DogImage = ({ breed }: DogImageProps) => {
 		return <p>Loading...</p>
 	}
 
-	if (error) {
-		return <p>Error: {error.message}</p>
-	}
 	if (!src) {
 		return <p>Imgage not exist</p>
 	}
 
-	return <Image src={src} width={500} height={500} alt={breed} loading="lazy" />
+	return (
+		<Image
+			src={src}
+			width={500}
+			height={500}
+			alt={breed}
+			loading="lazy"
+			unoptimized
+			onError={onError}
+		/>
+	)
 }
 
 export default DogImage
